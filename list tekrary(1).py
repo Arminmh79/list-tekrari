d131f682508381e00caf996f1b7f04bbb3d4938d
Armin <103656430+Arminmh79@users.noreply.github.com>
2022-04-28 12:01:53 +0430

    Add files via upload

---
+++ list tekrary(1).py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+def all_uniqe(lst):
+    return len(lst) == len(set(lst))  
+x = [1,1,2,2,3,2,3,4,5,6]
+y = [1,2,3,4,5]
+all_uniqe(x) #False
+all_uniqe(y) #True
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
